8a87211f72a888b2a64e7ad1dbb8e166b913394a
Scott Rice <meris608@gmail.com>
2012-01-22 19:42:51 -0600

    Added a method to create blocks with a custom title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
-  
+  def generate_block(title,&block)
+    title = '<div class="block big"><div class="titlebar"><h3>'.html_safe+title+'</h3><a href="#" class="toggle">&nbsp;</a></div>'.html_safe
+    body = '<div class="block_cont" style="display: block; ">'.html_safe+capture(&block)+'</div></div>'.html_safe
+    title+body
+  end
 end
