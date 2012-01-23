490e97eb2e2aa6c236fa1a50754fd071feeec3a0
Scott Rice <meris608@gmail.com>
2012-01-22 22:55:33 -0600

    Added name field to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120123034226) do
+ActiveRecord::Schema.define(:version => 20120123045317) do
 
   create_table "announcements", :force => true do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20120123034226) do
     t.datetime "created_at",                                            :null => false
     t.datetime "updated_at",                                            :null => false
     t.boolean  "admin"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
