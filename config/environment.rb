e4d5714e38ba0e6d5811257377cf1c37d45ea77f
Brian Gallagher <gallagh7@illinois.edu>
2012-02-03 17:42:59 -0600

    Added configuration settings in an attempt to get password reset mailer working

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Testapp::Application.initialize!
+
+# Configuration for using SendGrid on Heroku
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app2576834@heroku.com",
+  :password => "9ox3w2fj",
+  :domain => "phideillinois.heorkuapp.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
